1655ba402e98f5716f8ebd298b0fffbd714a56df
Italo souza sampaio <italo12346@gmail.com>
2026-04-14 09:06:09 -0300

    Fix: Erro de versao

--- app/src/consts/consts.js
+++
@@ -1,4 +0,0 @@
-export default consts = {
-  clienteId: 1,
-  salaoId: 1,
-}
--- app/src/store/modules/login/Types.ts
+++
@@ -1,8 +0,0 @@
-const types = {
-  LOGIN_REQUEST: '@login/LOGIN_REQUEST',
-  LOGIN_SUCCESS: '@login/LOGIN_SUCCESS',
-  LOGIN_FAILURE: '@login/LOGIN_FAILURE',
-  LOGOUT: '@login/LOGOUT',
-};
-
-export default types;
