788687caaf10c266b4b49ef9d5a7fe2847771a18
Italo souza sampaio <italo12346@gmail.com>
2026-04-15 09:39:19 -0300

    feat: ajustando api para retornar saloes proximos

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,8 +1,17 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 const apiUrl = process.env.REACT_APP_API_URL || "https://barba-corte.onrender.com";
 
 const api = axios.create({
   baseURL: apiUrl,
+});
+// Interceptor para injetar o token em todas as chamadas
+api.interceptors.request.use(async (config) => {
+  const token = await AsyncStorage.getItem('@barba-corte:token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
